66d6b8ffd00e5de2aa0bbe8797153e0ee0d76843
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2019-12-02 19:16:20 +0800

    Add informative subtitles

--- tito-jokes-app.py
+++ tito-jokes-app.py
@@ -4,18 +4,22 @@ import subprocess
 MAX_SAMPLES = 20
 CACHE_RESULTS = False
 
-"""# Ask Tito Joker anything! :)"""
+"""
+# Ask Tito Joker anything! :)
+ 
+ Created by [Lorenzo Ampil](https://www.linkedin.com/in/lorenzoampil/)
+"""
 num_tokens = st.sidebar.selectbox(
     'Token count for output',
      [10, 20, 40, 80, 160],
      index=2)
 
 num_samples = st.sidebar.selectbox(
-    'Number of candidate sequences to generate',
+    'Number of jokes to generate',
      list(range(1, MAX_SAMPLES + 1)),
      index=0)
 
-begin = st.text_input('Question', 'Why did the chicken cross the road?')
+begin = st.text_input('Ask any question', 'Why did the chicken cross the road?')
 
 def clean_joke(joke):
     return joke.replace('<soq>', '').replace('<eoa>', '').replace('<eoq>', '?').replace('<eoa', '')
@@ -29,11 +33,11 @@ def tell_joke(begin, num_samples, num_tokens=40):
         --num_samples={}\
         --stop_token="<eoj>"\
         --prompt="{}"
-        '''.format(num_tokens, num_samples, begin.replace('?', ''))
+        '''.format(num_tokens, num_samples, begin)
 
     out = subprocess.check_output(a, shell=True)
     jokes = (out.decode('utf-8').split('\n')[: -1])
-    processed_jokes = [begin + ' ' + clean_joke(joke) for joke in jokes]
+    processed_jokes = [begin.replace('?', '') + ' ' + clean_joke(joke) for joke in jokes]
     return processed_jokes
 
 jokes = tell_joke(begin, num_samples, num_tokens)
