c12faf4beef9947a74bc60869415b85230a3eb75
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2019-12-08 22:06:49 +0800

    Add automated GIPHY integration w/ NER and POS ML

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 wget https://storage.googleapis.com/joke-generator-model1/model1.zip
 unzip model1.zip
 pip install -r requirements.txt
+python -m spacy download en_core_web_sm
--- tito-jokes-app.py
+++ tito-jokes-app.py
@@ -6,41 +6,80 @@ import pandas as pd
 from datetime import datetime
 import os
 from random import randrange
+import spacy
+import giphy_client
+from giphy_client.rest import ApiException
+
+nlp = spacy.load("en_core_web_sm")
 
 CONFIG = "config.yaml"
 SOURCE = "model1.zip"
 TARGET = "model1.zip"
 BUCKET = "joke-generator-model1"
 MAX_SAMPLES = 20
-DEFAULT_QUESTION = 'Why did the chicken cross the road?'
+DEFAULT_QUESTION = "Why did the chicken cross the road?"
 SEED_GENERATOR = False
+
 
 class Struct:
     """
     Class to convert a dictionary into an object where values are accessible as atttributes
     """
+
     def __init__(self, **entries):
         self.__dict__.update(**entries)
-        
+
     def __repr__(self):
-        return '\n'.join(["{}: {}".format(k, v) for k, v in self.__dict__.items()])
+        return "\n".join(["{}: {}".format(k, v) for k, v in self.__dict__.items()])
+
+
+def get_giphy(
+    q,  # str | Search query term or prhase.
+    api_key="dc6zaTOxFJmzC",  # str | Giphy API Key.
+    limit=1,  # int | The maximum number of records to return. (optional) (default to 25)
+    offset=0,  # int | An optional results offset. Defaults to 0. (optional) (default to 0)
+    rating="g",  # str | Filters results by specified rating. (optional)
+    lang="en",  # str | Specify default country for regional content; use a 2-letter ISO 639-1 country code. See list of supported languages <a href = \"../language-support\">here</a>. (optional)
+    fmt="json",
+):
+    try:
+        api_instance = giphy_client.DefaultApi()
+        # Search Endpoint
+        api_response = api_instance.gifs_search_get(
+            api_key, q, limit=limit, offset=offset, rating=rating, lang=lang, fmt=fmt
+        )
+        first_result_id = api_response.to_dict()["data"][0]["id"]
+        return "https://media.giphy.com/media/{}/giphy.gif".format(first_result_id)
+    except ApiException as e:
+        print("Exception when calling DefaultApi->gifs_search_get: %s\n" % e)
+
+
+def get_tokens(text, pos=["NOUN"]):
+    """
+    Return tokens with a specified list of pos tags
+    """
+    doc = nlp(text)
+    return [t for t in doc if t.pos_ in pos]
+
 
 def update_csv(df, fp):
-    # if file does not exist write header 
+    # if file does not exist write header
     if not os.path.isfile(fp):
         print("File ({}) not found!")
-        df.to_csv(fp, header='column_names', index=False)
+        df.to_csv(fp, header="column_names", index=False)
         print("File ({}) created!")
-    else: # else it exists so append without writing the header
+    else:  # else it exists so append without writing the header
         print("File ({}) found!")
-        df.to_csv(fp, mode='a', header=False, index=False)
+        df.to_csv(fp, mode="a", header=False, index=False)
         print("File ({}) updated!")
-       
+
+
 def make_directory(directory):
     if not os.path.exists(directory):
-        print('Directory ({}) not found!'.format(directory))
+        print("Directory ({}) not found!".format(directory))
         os.makedirs(directory)
-        print('Directory ({}) created!'.format(directory))
+        print("Directory ({}) created!".format(directory))
+
 
 def read_yaml(fp):
     """
@@ -48,78 +87,95 @@ def read_yaml(fp):
     """
     with open(fp) as f:
         y = yaml.load(f, Loader=yaml.FullLoader)
-        
+
     gen_config = Struct(**y)
     return gen_config
 
+
 def get_config(fp):
     args = read_yaml(fp)
-    args.device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+    args.device = torch.device(
+        "cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu"
+    )
     args.n_gpu = torch.cuda.device_count()
     args.model_type = args.model_type.lower()
     return args
 
+
 def clean_joke(joke):
-    joke = joke.replace('<eoq>', '?').strip()
-    return ' '.join([t for t in joke.split() if t[0] != '<' and t[-1] != '>'])
+    joke = joke.replace("<eoq>", "?").strip()
+    return " ".join([t for t in joke.split() if t[0] != "<" and t[-1] != ">"])
+
 
 def tell_joke(args):
     model, tokenizer = read_model_tokenizer(args)
     jokes = generate_text(args, model, tokenizer)
-    processed_jokes = [args.prompt + ' ' + joke for joke in jokes]
+    processed_jokes = [args.prompt + " " + joke for joke in jokes]
     return processed_jokes
 
-if __name__=='__main__':
 
-    st.markdown("""
+if __name__ == "__main__":
+
+    st.markdown(
+        """
     # Ask Tito Joker anything! :)
     
     Created by [Lorenzo Ampil](https://medium.com/@lorenzo.ampil).
     
-    """)
+    """
+    )
     num_tokens = st.sidebar.selectbox(
-        'Token count for output',
-        [10, 20, 40, 80, 160],
-        index=2)
+        "Token count for output", [10, 20, 40, 80, 160], index=2
+    )
 
     num_samples = st.sidebar.selectbox(
-        'Number of jokes to generate',
-        list(range(1, MAX_SAMPLES + 1)),
-        index=0)
-    
-    st.sidebar.markdown("Send feature requests [here](https://forms.gle/yuivvdpQxgRGoq238).")
+        "Number of jokes to generate", list(range(1, MAX_SAMPLES + 1)), index=0
+    )
 
-    begin = st.text_input('Ask any question', DEFAULT_QUESTION)
-    
+    st.sidebar.markdown(
+        "Send feature requests [here](https://forms.gle/yuivvdpQxgRGoq238)."
+    )
+
+    begin = st.text_input("Ask any question", DEFAULT_QUESTION)
+
     args = get_config(CONFIG)
-    args.prompt = begin.replace('?', '')
+    args.prompt = begin.replace("?", "")
     args.length = num_tokens
     args.num_samples = num_samples
-    
+
     if not SEED_GENERATOR:
         # This ensures that samples are unique, even with similar prompts
         args.seed = randrange(10000)
-    
+
     print(args)
-    current_timestamp = datetime.strftime(datetime.utcnow(), '%Y-%m-%dT%H:%M:%S')
+    current_timestamp = datetime.strftime(datetime.utcnow(), "%Y-%m-%dT%H:%M:%S")
     jokes = tell_joke(args)
 
-    enumerated_jokes = [str(i + 1) + '. ' + clean_joke(joke) for i, joke in enumerate(jokes)]
-    st.write('\n'.join(enumerated_jokes))
-    
-    make_directory('data')
+    enumerated_jokes = [
+        str(i + 1) + ". " + clean_joke(joke) for i, joke in enumerate(jokes)
+    ]
+    st.write("\n".join(enumerated_jokes))
+
+    # Return gif based on first common noun found (if at least 1 common noun found)
+    jokes_nouns = get_tokens(jokes[0])
+    if jokes:
+        giphy_url = get_giphy(jokes_nouns[0].text)
+        print("Giphy url:", giphy_url)
+        st.markdown("![Alt Text]({})".format(giphy_url))
+
+    make_directory("data")
     # Split jokes in question & answer.
     split_jokes = pd.DataFrame([j.split("<eoq>") for j in jokes]).fillna("")
-    
+
     # Only save the joke if it's not the default question and at least one of the jokes have a complete question
     if split_jokes.shape[1] == 2:
         # Then, save as a csv.
-        split_jokes.columns = ['question', 'answer']
-        split_jokes['timestamp_utc'] = current_timestamp
-        split_jokes['prompt'] = args.prompt
+        split_jokes.columns = ["question", "answer"]
+        split_jokes["timestamp_utc"] = current_timestamp
+        split_jokes["prompt"] = args.prompt
         print(split_jokes)
         split_jokes = split_jokes.applymap(clean_joke)
-        split_jokes_fn = './data/jokes_generated.csv'
-        update_csv(split_jokes, split_jokes_fn)        
-        
-        print('Generated jokes updated to {} !'.format(split_jokes_fn))
+        split_jokes_fn = "./data/jokes_generated.csv"
+        update_csv(split_jokes, split_jokes_fn)
+
+        print("Generated jokes updated to {} !".format(split_jokes_fn))
