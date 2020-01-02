27d015aa2de15377f2d3d9d51a68e31c536de202
Lorenzo Ampil <lorenzojulioampil@gmail.com>
2020-01-02 21:53:18 +0800

    Apply renaming of app on bash script

--- run_tito_joker.sh
+++ run_tito_joker.sh
@@ -1,4 +1,4 @@
-until streamlit run tito-jokes-app.py; do
+until streamlit run tito_joker_app.py; do
             echo "Tito Joker crashed with exit code $?.  Respawning.." >&2
                 sleep 1
         done
